6a03d5f691bf2dcf573ce82da70f904db12d4154
Lily Udovyk <pelinknew@gmail.com>
2020-08-26 14:17:09 +0300

    Updates

---
+++ precache-manifest.27642668a0e6bacbd9a7fab88de0a1b9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bf49b9d14fdc6b5c9cec3c5138750b5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6b671dcbf578236dfc02",
+    "url": "./static/css/3.e347c675.chunk.css"
+  },
+  {
+    "revision": "9ad0c914f5c841a5fb76",
+    "url": "./static/css/4.915e2d1c.chunk.css"
+  },
+  {
+    "revision": "66edea7bfffb1f177d4b",
+    "url": "./static/css/5.450872c0.chunk.css"
+  },
+  {
+    "revision": "0fb7e1c98b7481cca686",
+    "url": "./static/css/6.e491c835.chunk.css"
+  },
+  {
+    "revision": "b84beb3157591758132e",
+    "url": "./static/css/main.9166c390.chunk.css"
+  },
+  {
+    "revision": "7681174a13d50f7b2f8e",
+    "url": "./static/js/2.d2c00722.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.d2c00722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b671dcbf578236dfc02",
+    "url": "./static/js/3.5c4af158.chunk.js"
+  },
+  {
+    "revision": "9ad0c914f5c841a5fb76",
+    "url": "./static/js/4.a3c78b15.chunk.js"
+  },
+  {
+    "revision": "66edea7bfffb1f177d4b",
+    "url": "./static/js/5.a9e75fce.chunk.js"
+  },
+  {
+    "revision": "0fb7e1c98b7481cca686",
+    "url": "./static/js/6.80ea1ceb.chunk.js"
+  },
+  {
+    "revision": "b84beb3157591758132e",
+    "url": "./static/js/main.757b4259.chunk.js"
+  },
+  {
+    "revision": "27e7b97811a8817b7a88",
+    "url": "./static/js/runtime-main.0d09e278.js"
+  }
+]);
